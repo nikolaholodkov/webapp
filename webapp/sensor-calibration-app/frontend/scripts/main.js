70feb3d5ec3cf21a42699ba47d35e7d3681f81ff
Nikola Holodkov <nholodkov@ogs.it>
2025-04-28 17:35:35 +0200

    First page almost done

---
+++ webapp/sensor-calibration-app/frontend/scripts/main.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+  const dropdown = document.getElementById('dropdown-menu');
+  if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+    dropdown.style.display = 'block';
+  } else {
+    dropdown.style.display = 'none';
+  }
+}
