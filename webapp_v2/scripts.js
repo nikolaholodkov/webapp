f1bb5d777bcbb8dbadc6976421d79706de64aa2c
Nikola Holodkov <nholodkov@ogs.it>
2025-04-29 16:52:05 +0200

    Second page almost done!

---
+++ webapp_v2/scripts.js
@@ -0,0 +1,14 @@
+function saveData() {
+  // Add your save logic here
+  alert('Data saved successfully!');
+}
+
+function goToPreviousPage() {
+  // Replace 'previous-page.html' with the actual URL of the previous page
+  window.location.href = 'index.html';
+}
+
+function goToNextPage() {
+  // Replace 'next-page.html' with the actual URL of the next page
+  window.location.href = 'secondpage.html';
+}
